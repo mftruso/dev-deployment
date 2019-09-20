6b67339b847b32c9ab0b56e31c4ec3ff501e7f10
skipwe <skipwelsh1@gmail.com>
2019-09-20 10:48:59 -0500

    Swich setup to https

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -9,17 +9,17 @@ fi
 . ./bin/all.sh
 pushd .. > /dev/null;
 
-git clone git@github.com:servantscode/java-commons.git
-git clone git@github.com:servantscode/docker-elk.git
-git clone git@github.com:servantscode/tomcat-elk-logging.git
-git clone git@github.com:servantscode/fake-data.git
+git clone https://github.com/servantscode/java-commons.git
+git clone https://github.com/servantscode/docker-elk.git
+git clone https://github.com/servantscode/tomcat-elk-logging.git
+git clone https://github.com/servantscode/fake-data.git
 
 echo Pulling Servant\'s Code repos: $PROJECTS
 
 for dir in $PROJECTS; do
   echo;
   echo "Cloning $dir";
-  git clone git@github.com:servantscode/$dir.git
+  git clone https://github.com/servantscode/$dir.git
 done
 
 cp dev-deployment/bin/*.sh .
