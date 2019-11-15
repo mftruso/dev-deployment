47b4de92241e76f0c1feb1416c47f94386811529
Greg Leitheiser <greg@servantscode.org>
2019-11-15 16:58:18 -0600

    PushPay integration. Mark 1: Authorization. Plus initial 3rd party integration controls

--- bin/all.sh
+++ bin/all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export PROJECTS="person-svc ministry-svc permission-svc schedule-svc donation-svc metrics-svc photo-svc note-svc email-svc sacrament-svc parish-svc search-svc formation-svc parish-manager-ui"
+export PROJECTS="person-svc ministry-svc permission-svc schedule-svc donation-svc metrics-svc photo-svc note-svc email-svc sacrament-svc parish-svc search-svc formation-svc integration-svc parish-manager-ui parishioner-ui"
 
